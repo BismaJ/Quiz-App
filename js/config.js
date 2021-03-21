d060a8fbf533e1966d4f4a607051d57c26426640
Bisma Jamil <42489199+BismaJ@users.noreply.github.com>
2021-03-22 00:35:38 +0500

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,24 @@
+var introVideo = 'media/logo_reveal.mp4';
+var rightAnswer = 'Good, right answer!';
+var wrongAnswer = 'Sorry, wrong answer!';
+var playNowButton = 'PLAY NOW';
+var skipButtonText = 'NEXT QUESTION';
+var playAgainButton = 'PLAY AGAIN';
+
+var quizzes = [{
+    question: `SSN is being shutdown in May. What new collaboration tool is coming to Siemens?`,
+    choiceA: `Facebook`,
+    choiceB: `Instagram`,
+    choiceC: `Yammer`,
+    choiceD: `Pinterest`,
+    correct: 'C',
+    didYouKnow: `Yammer is a powerful collaboration tool provided by Microsoft and included in the Office 365 package.`,
+}, {
+    question: `What's the most efficient way to take all my files on the go?`,
+    choiceA: `By using Syncplicity mobile app.`,
+    choiceB: `By printing them.`,
+    choiceC: `By memorizing them.`,
+    choiceD: `By sending them my e-mail.`,
+    correct: 'A',
+    didYouKnow: `Syncplicity mobile app is available for both iOs and Android devices in your local play store or in Siemens catalog app.`,
+}]
